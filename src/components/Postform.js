3291bee03068be8fe9386774287dd8576dac7199
shaz <imshazbangash@gmail.com>
2019-01-07 23:10:02 +0300

    Fetch API

---
+++ src/components/Postform.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+
+class Postform extends Component {
+
+
+constructor(props){
+    super(props); 
+    this.state ={
+
+        title:'', 
+        body:''
+    }
+}
+
+ onChange = (e) => {
+
+    this.setState({
+
+        [e.target.name]:  e.target.value,
+    })
+
+}
+
+onSubmit =(e)=> {
+
+    e.preventDefault(); 
+
+    const post ={
+        title: this.state.title, 
+        body: this.state.body
+    }
+    fetch('https://jsonplaceholder.typicode.com/posts',
+        {
+            method:'POST',
+            headers:{
+                'content-type': 'appliation/json'
+            }, 
+            body: JSON.stringify(post)
+        }
+    ).then(res =>res.json())
+    .then(data=>console.log(data))
+}
+  render() {
+    return (
+      <div>
+        <h1> Add post </h1> 
+
+        <form onSubmit={this.onSubmit}> 
+            <div> 
+                <label> Title: </label> <br/> 
+                <input type="text" name="title" onChange={this.onChange} value={this.state.title}/>
+            </div> <br/> 
+            <div> 
+                <label> body: </label> <br/> 
+                <textarea  onChange={this.onChange} value={this.state.body} name="body"/>
+            </div><br/> 
+            <button type="submit" > Submit </button> 
+        </form> 
+      </div>
+    )
+  }
+}
+
+
+export default Postform; 
